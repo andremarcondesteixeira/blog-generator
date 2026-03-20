2cb3c1230881bc385d2cb6044a8e370217f916d8
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2026-03-20 18:31:18 -0300

    Imports blog_generator and transfer ownership or Args

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod args;
 pub use args::Args;
 
-pub fn run(_args: &Args) -> Result<(), String> {
+pub fn run(_args: Args) -> Result<(), String> {
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+use blog_generator::{run, Args};
 use std::process;
 
 fn main() {
-    let args = blog_generator::Args::from_command_line_arguments().unwrap_or_else(|e| {
+    let args = Args::from_command_line_arguments().unwrap_or_else(|e| {
         eprintln!("Error: {e}");
         process::exit(1);
     });
-    
-    if let Err(e) = blog_generator::run(&args) {
+
+    if let Err(e) = run(args) {
         eprintln!("Error: {e}");
         process::exit(1);
     }
